a6d732dce9fe762cb4c5b82553c0a72e294d1d91
Robson P Oliveira <rpopuc@gmail.com>
2019-07-08 13:23:12 -0300

    Adiciona upload de arquivos
    - Somente para usuários registrados
    - Closes #123

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 // Obtém o middleware de autenticação
 import authMiddleware from './app/middlewares/auth';
@@ -20,5 +23,9 @@ routes.post('/sessions', SessionController.store);
 
 // Atualização dos dados do usuário
 routes.put('/users', authMiddleware, UserController.update);
+
+// Upload de arquivos
+const upload = multer(multerConfig);
+routes.post('/files', authMiddleware, upload.single('file'), FileController.store);
 
 export default routes;
