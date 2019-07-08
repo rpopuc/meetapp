9fe2857bcb9f90f169acae09a8c4611322f96aba
Robson P Oliveira <rpopuc@gmail.com>
2019-07-08 13:38:55 -0300

    Adiciona migration para meetups

---
+++ src/database/migrations/20190708163050-create-meetups.js
@@ -0,0 +1,66 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('meetups', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+
+      location: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+
+      banner_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'files', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+
+      canceled_at: {
+        type: Sequelize.DATE,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('meetups');
+  },
+};
