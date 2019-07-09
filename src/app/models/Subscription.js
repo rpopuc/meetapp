e15e4fc706f0d8b235d8ba00be4cefc8a94ebe40
Robson P Oliveira <rpopuc@gmail.com>
2019-07-09 08:04:10 -0300

    Adiciona model para subscription

---
+++ src/app/models/Subscription.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Subscription extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        // Nada
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Meetup, {
+      foreignKey: 'meetup_id',
+    });
+
+    this.belongsTo(models.User, {
+      foreignKey: 'user_id',
+    });
+  }
+}
+
+export default Subscription;
